185301d115a0f274a700a9a8891a4edf30fa1432
Shelda <sdelassin@gmail.com>
2021-02-28 03:41:30 -0500

    add more methods to UserService

--- src/main/java/com/tts/tttwitter/service/UserService.java
+++ src/main/java/com/tts/tttwitter/service/UserService.java
@@ -1,11 +1,15 @@
 package com.tts.tttwitter.service;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.tts.tttwitter.model.Role;
 import com.tts.tttwitter.model.User;
 import com.tts.tttwitter.repository.RoleRepository;
 import com.tts.tttwitter.repository.UserRepository;
@@ -37,4 +41,20 @@ public class UserService {
     public void save(User user) {
         userRepository.save(user);
     }
+    
+    public User saveNewUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("USER");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+    
+    public User getLoggedInUser() {
+        String loggedInUsername = SecurityContextHolder.
+          getContext().getAuthentication().getName();
+        
+        return findByUsername(loggedInUsername);
+    }
+    
 }
